c5ac83034c20dd11c7a898a2ab6b20f8f6fbd312
ben <benwestondigital@gmail.com>
2024-06-05 08:14:17 +0100

    feat: hsl colours randomized

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
+
+type HslColour = `hsl(${number}, ${number}%, ${number}%)`;
+
+const randomColour = (): HslColour => {
+  const h = Math.floor(Math.random() * 360);
+  const s = Math.floor(Math.random() * 100);
+  const l = Math.floor(Math.random() * 100);
+  return `hsl(${h}, ${s}%, ${l}%)`;
+};
 
 export const App = () => {
-  return <div>hello</div>;
+  const [colour, setColour] = useState<HslColour>(`hsl(0, 0%, 0%)`);
+
+  return (
+    <div style={{ backgroundColor: colour }} className="colour">
+      <button onClick={() => setColour(randomColour())}>click me</button>
+    </div>
+  );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,42 @@
-import { resolve, join } from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-export const entry = './src/index.tsx';
-export const output = {
-  path: resolve(__dirname, 'dist'),
-  filename: 'bundle.js',
-};
-export const resolve = {
-  extensions: ['.js', '.jsx', '.ts', '.tsx'],
-};
-export const module = {
-  rules: [
-    {
-      test: /\.(ts|tsx)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-      },
-    },
-  ],
-};
-export const plugins = [
-  new HtmlWebpackPlugin({
-    template: './public/index.html',
-    filename: 'index.html',
-  }),
-];
-export const devServer = {
-  static: {
-    directory: join(__dirname, 'public'),
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
-  compress: true,
-  port: 3000,
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 3000,
+  },
+  devtool: 'source-map',
 };
-export const devtool = 'source-map';
